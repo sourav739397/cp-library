394ce6bc78c79b72d72cf45edf5713e6389e6cbf
sourav739397 <sourav739397@gmail.com>
2025-10-16 03:31:08 +0600

    upd bit

--- content/data-structures/1D Range Queries/FenwickTree.h
+++ content/data-structures/1D Range Queries/FenwickTree.h
@@ -4,7 +4,7 @@
  * License: CC0
  * Source: folklore/TopCoder
  * Description: Computes partial sums a[0] + a[1] + ... + a[pos - 1], and updates single elements a[i],
- * taking the difference between the old and new value.
+ * taking the difference between the old and new value. 0-Indexed.
  * Time: Both operations are $O(\log N)$.
  * Status: Stress-tested
  */
@@ -13,10 +13,12 @@
 template<class T> struct FenwickTree {
 	int N; vector<T> bit;
 	FenwickTree(int _N) { N = _N; bit.resize(N); }
-	void add(int p, T x) { // a[p] += x
-    for (++p; p <= N; p += p&-p) bit[p-1] += x; 
+	void add(int p, T x) { 
+    for (++p; p <= N; p += p&-p) {
+      bit[p-1] += x; 
+    }
   }
-	T sum(int l, int r) { return sum(r+1)-sum(l); } // Zero indexed
+	T sum(int l, int r) { return sum(r+1)-sum(l); }
 	T sum(int r) { 
     T res = 0; 
     for(; r; r -= r&-r) res += bit[r-1]; 
--- content/data-structures/1D Range Queries/SegmentTree.h
+++ content/data-structures/1D Range Queries/SegmentTree.h
@@ -14,9 +14,9 @@ template<class T> struct SegTree {
   int n; vector<T> seg;
   SegTree(int _n) {
     for (n = 1; n < _n; ) n *= 2;
-    seg.assign(2 * n, ID);
+    seg.assign(2*n, ID);
   }
-  void pull(int p) { seg[p] = cmb(seg[2 * p], seg[2 * p + 1]); }
+  void pull(int p) { seg[p] = cmb(seg[2*p], seg[2*p+1]); }
   void update(int p, T val) { // set val at position p
     seg[p += n] = val;
     for (p /= 2; p; p /= 2) pull(p);
