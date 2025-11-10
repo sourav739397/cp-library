69908415e2e7c7c2d8c8866bb211ec0228f438e2
sourav739397 <sourav739397@gmail.com>
2025-11-10 11:12:24 +0600

    upd mint

--- content/number-theory/Modular Arithmetic/ModIntShort.h
+++ content/number-theory/Modular Arithmetic/ModIntShort.h
@@ -2,11 +2,12 @@
  * Description: Modular arithmetic. Assumes $MOD$ is prime.
  * Source: KACTL
  * Verification: https://open.kattis.com/problems/modulararithmetic
- * Usage: mi a = MOD+5; inv(a); // 400000003
+ * Usage: mint a = MOD+5; inv(a); // 400000003
  */
 
-template<int MOD = (int)1E9+7> struct mint {
+struct mint {
   int v;
+  static const int MOD = 1E9 + 7;
   explicit operator int() const { return v; }
   mint() : v(0) {}
   mint(int64_t _v) : v(int(_v % MOD)) { v += (v < 0) * MOD; }
