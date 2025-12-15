b972d13d7f2d02e3de0290bcb2e6399fd5e5248a
sourav739397 <sourav739397@gmail.com>
2025-12-15 10:51:36 +0600

    color 0/1 -> 1/2

--- content/graphs/Basics/Bipartite.h
+++ content/graphs/Basics/Bipartite.h
@@ -1,9 +1,9 @@
 /**
  * Description: Checks if an undirected graph is bipartite.
-  * Colors nodes with 0/1. If not bipartite, returns false.
+  * Colors nodes with 1/2. If not bipartite, returns false.
  * Time: O(N + M)
  * Source: own
- * Verification: https://www.spoj.com/problems/BUGLIFE/
+ * Verification: https://cses.fi/problemset/task/1668/
  */
 
 struct Bipartite {
@@ -13,7 +13,7 @@ struct Bipartite {
   Bipartite(int _SZ) {
     SZ = _SZ;
     adj.resize(SZ);
-    color.resize(SZ, -1);
+    color.resize(SZ);
   }
   void ae(int u, int v) {
     adj[u].push_back(v);
@@ -22,13 +22,13 @@ struct Bipartite {
   bool check() {
     queue<int> todo;
     for (int src = 0; src < SZ; src++) {
-      if (color[src] != -1) continue;
+      if (color[src]) continue;
       color[src] = 1; todo.push(src);
       while (!todo.empty()) {
         int u = todo.front(); todo.pop();
         for (auto& v: adj[u]) {
-          if (color[v]) color[v] = 1^color[u]; todo.push(v);
           if (color[v] == color[u]) return false;
+          if (!color[v]) color[v] = 3-color[u], todo.push(v);
         }
       }
     }
