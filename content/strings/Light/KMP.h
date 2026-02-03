eb4cc1edac78689abef4cd6170ef4d0be32c239e
sourav739397 <sourav739397@gmail.com>
2026-02-03 22:05:53 +0600

    remove macros

--- content/strings/Light/KMP.h
+++ content/strings/Light/KMP.h
@@ -1,21 +1,28 @@
 /**
- * Source: KACTL
- * Time: O(N)
  * Description: \texttt{f[i]} is length of the longest proper suffix 
  	* of the $i$-th prefix of $s$ that is a prefix of $s$
- * Verification: https://open.kattis.com/problems/stringmatching
+ * Source: KACTL
+ * Time: O(N)
+ * Verification: https://cses.fi/problemset/task/1753/
  */
 
-vi kmp(str s) {
-	int N = sz(s); vi f(N+1); f[0] = -1;
-	FOR(i,1,N+1) {
-		for (f[i]=f[i-1];f[i]!=-1&&s[f[i]]!=s[i-1];)f[i]=f[f[i]];
-		++f[i]; }
-	return f;
+vector<int> KMP(string s) {
+  int N = s.size();
+  vector<int> f(N + 1); f[0] = -1;
+  for (int i = 1; i <= N; i++) {
+    for (f[i] = f[i-1]; f[i] != -1 && s[f[i]] != s[i-1];) {
+      f[i] = f[f[i]];
+    }
+    ++f[i];
+  }
+  return f;
 }
-vi getOc(str a, str b) { // find occurrences of a in b
-	vi f = kmp(a+"@"+b), ret;
-	FOR(i,sz(a),sz(b)+1) if (f[i+sz(a)+1] == sz(a)) 
-		ret.pb(i-sz(a));
-	return ret;
+vector<int> getOc(string a, string b){ // find occurrences of a in b
+  vector<int> f = KMP(a + "@" + b), res;
+  for (int i = a.size(); i <= b.size(); i++) {
+    if (f[i+a.size()+1] == a.size()) {
+      res.push_back(i - a.size());
+    }
+  }
+  return res;
 }
