939424d4ffee0a1ffe6ca15e5b2659f4c268fd1d
sourav739397 <sourav739397@gmail.com>
2026-05-17 16:46:22 +0600

    ,

--- Implementations/content/combinatorial/DeBruijnSequence.h
+++ Implementations/content/combinatorial/DeBruijnSequence.h
@@ -48,7 +48,16 @@ template<class T> struct SegmentTree {
 vector<int> deBruijnSequence(int k, int n) {
   if (k == 1) return {0};
   vector<int> seq, aux(n+1);
-  
+  auto gen = [&](this auto&& self, int t, int p)->void {
+    if (t > n) {
+      if (n % p == 0) for (int i = 1; i <= p; i++) seq.push_back(aux[i]);
+    }
+    else {
+      aux[t] = aux[t-p]; self(t+1, p);
+      while (++aux[t] < k) self(t+1, t);
+    }
+  };
+  gen(1, 1); return seq;
 }
 
 void solve() {
