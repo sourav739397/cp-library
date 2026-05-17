478505b63e7d46dbcde28f5c8d4922b922c940ef
sourav739397 <sourav739397@gmail.com>
2026-05-17 11:22:00 +0600

    .

--- content/number-theory (11.1)/Primality/FactorBasic.h
+++ content/number-theory (11.1)/Primality/FactorBasic.h
@@ -6,40 +6,55 @@
  */
 
 inline namespace factorBasic {
-template<class T> vector<pair<T,int>> factor(T x) { 
-	vector<pair<T,int>> pri;
-	for (T i = 2; i*i <= x; ++i) if (x % i == 0) {
-		int t = 0;
-		while (x % i == 0) x /= i, t ++;
-		pri.pb({i,t});
-	}
-	if (x > 1) pri.pb({x,1});
-	return pri;
+template<class T> vector<pair<T, int>> factor(T x) {
+  vector<pair<T, int>> primes;
+  for (T i = 2; i*i <= x; ++i) if (x%i == 0) {
+    int t = 0;
+    while (x%i == 0) x /= i, t++;
+    primes.push_back({i, t});
+  }
+  if (x > 1) primes.push_back({x, 1});
+  return primes;
 }
+
 /* Note:
  * number of operations needed s.t.
  *				  phi(phi(...phi(n)...))=1
  * is O(log n).
  * Euler's theorem: a^{\phi(p)}\equiv 1 (mod p), gcd(a,p)=1
  */
-ll phi(ll x) {
-	each(a,factor(x)) x -= x/a.f;
+template<class T> T phi(T x) {
+	for (auto& [p, exp]: factor(x)) x -= x/p;
 	return x;
 }
-template<class T> void tour(vector<pair<T,int>>& v, 
-	vector<T>& V, int ind, T cur) {
-		if (ind == sz(v)) V.pb(cur);
-		else {
-			T mul = 1;
-			F0R(i,v[ind].s+1) {
-				tour(v,V,ind+1,cur*mul);
-				mul *= v[ind].f;
-			}
-		}
-	}
-template<class T> vector<T> getDivi(T x) {
-	auto v = factor(x);
-	vector<T> V; tour(v,V,0,(T)1); sort(all(V));
-	return V;
+
+template<class T> void tour(vector<pair<T, int>> &v, 
+vector<T> &V, int ind, T cur) {
+  if (ind == (int)size(v)) V.push_back(cur);
+  else {
+    T mul = 1;
+    for (int i = 0; i <= v[ind].second; i++) {
+      tour(v, V, ind+1, cur*mul);
+      mul *= v[ind].first;
+    }
+  }
+}
+
+template<class T> vector<T> getDiv(T x) {
+  auto v = factor(x);
+  vector<T> V;
+  tour(v, V, 0, (T)1);
+  ranges::sort(V);
+  return V;
+}
+
+template<class T> vector<T> getDiv(T x) {
+  vector<T> V;
+  for (T i = 1; i*i <= x; ++i) if (x%i == 0) {
+    V.push_back(i);
+    if (x/i != i) V.push_back(x/i);
+  }
+  ranges::sort(V);
+  return V;
 }
 }
