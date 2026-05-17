8be8b879595c440a77bee0853eddfd44ab77b10c
sourav739397 <sourav739397@gmail.com>
2026-05-17 16:38:30 +0600

    .

--- Implementations/content/combinatorial/DeBruijnSeq.h
+++ Implementations/content/combinatorial/DeBruijnSeq.h
@@ -1,26 +1,18 @@
-/**
- * Description: Given alphabet $[0,k)$ constructs a cyclic string
- * of length $k^n$ that contains every length $n$ string as substr.
- * Source: https://github.com/koosaga/DeobureoMinkyuParty/blob/master/teamnote.tex
- * https://en.wikipedia.org/wiki/De_Bruijn_sequence
- * pg 241 of http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.93.5967&rep=rep1&type=pdf
- * Verification: https://cses.fi/problemset/task/1692/
- */
+#include <bits/stdc++.h>
+using namespace std;
 
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...)
+#endif
 
-vector<int> deBruijnSequence(int k, int n) {
-	if (k == 1) return {0};
-	vector<int> seq, aux(n + 1);
-	auto gen = [&](auto &&self, int t, int p) -> void {
-		if (t > n) {
-			if (n % p == 0) for (int i = 1; i <= p; i++) {
-				seq.push_back(aux[i]);
-			}
-		}
-		else {
-			aux[t] = aux[t-p]; self(self, t+1, p);
-			while (++aux[t] < k) self(self, t+1, t);
-		}
-	};
-	gen(gen, 1, 1); return seq;
+void solve() {
+	cout << "Hello World!\n";
+}
+
+int32_t main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	solve();
 }
