bd6a52dbbf37cdde1305b85ba3f8a629c782133e
sourav739397 <sourav739397@gmail.com>
2026-05-17 19:11:25 +0600

    .

--- Implementations/content/graphs (12)/Basics/BellmanFord.h
+++ Implementations/content/graphs (12)/Basics/BellmanFord.h
@@ -13,29 +13,28 @@ struct BellmanFord {
   BellmanFord(int _N) {
     N = _N;
     adj.resize(N);
-    dist.resize(N, INT64_MAX);
+    dist.resize(N, INF);
   }
   void ae(int u, int v, int w) {
     adj[u].push_back(v);
     ed.push_back({u, v, w});
   }
   void genBad(int u) {
-    if (dist[u] == INT64_MIN) return;
-    dist[u] = INT64_MIN;
+    if (dist[u] == -INF) return;
+    dist[u] = -INF;
     for (auto &v : adj[u]) genBad(v);
   }
   void genDist(int src) {
     dist[src] = 0;
     for (int i = 0; i < N; i++) {
       for (auto &[u, v, w] : ed) {
-        if (dist[u] < INT64_MAX) {
-          dist[v] = min(dist[v], dist[u] + w);
-				}
-			}
-		}
+        if (dist[u] < INF) {
+          dist[v] = min(dist[v], dist[u]+w);
+        }
+      }
+    }
     for (auto &[u, v, w] : ed) {
-			if (dist[u] < INT64_MAX && dist[v] > dist[u] + w)
-				genBad(v);
-		}
-	}
+      if (dist[u] < INF && dist[v] > dist[u]+w) genBad(v);
+    }
+  }
 };
