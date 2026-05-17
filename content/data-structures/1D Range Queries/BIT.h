bae05fd3f6fbe6aca110b88f5316cfe5e9dd65a4
sourav739397 <sourav739397@gmail.com>
2026-05-17 11:47:58 +0600

    .

--- content/data-structures/1D Range Queries/BIT.h
+++ content/data-structures/1D Range Queries/BIT.h
@@ -1,28 +1,32 @@
 /**
- * Author: Lukas Polacek
- * Date: 2009-10-30
- * License: CC0
- * Source: folklore/TopCoder
  * Description: Computes partial sums a[0] + a[1] + ... + a[pos - 1], and updates single elements a[i],
- * taking the difference between the old and new value.
+	* taking the difference between the old and new value. 0-Indexed.
  * Time: Both operations are $O(\log N)$.
- * Status: Stress-tested
  */
 #pragma once
 
-tcT> struct BIT {
-	int N; V<T> data;
-	void init(int _N) { N = _N; data.rsz(N); }
-	void add(int p, T x) { for (++p;p<=N;p+=p&-p) data[p-1] += x; }
+template<class T> struct BIT {
+	int N; vector<T> bit;
+	BIT(int _N) {
+		N = _N;
+		bit.resize(N);
+	}
+	void add(int p, T x) {
+		for (++p; p <= N; p += p&-p) bit[p - 1] += x;
+	}
 	T sum(int l, int r) { return sum(r+1)-sum(l); }
-	T sum(int r) { T s = 0; for(;r;r-=r&-r)s+=data[r-1]; return s; }
+	T sum(int r) {
+		T res = 0;
+		for (; r; r -= r&-r) res += bit[r - 1];
+		return res;
+	}
 	int lower_bound(T sum) {
 		if (sum <= 0) return -1;
 		int pos = 0;
-		for (int pw = 1<<25; pw; pw >>= 1) {
-			int npos = pos+pw;
-			if (npos <= N && data[npos-1] < sum)
-				pos = npos, sum -= data[pos-1];
+		for (int pw = 1 << 25; pw; pw >>= 1) {
+			int npos = pos + pw;
+			if (npos <= N && bit[npos - 1] < sum)
+				pos = npos, sum -= bit[pos - 1];
 		}
 		return pos;
 	}
