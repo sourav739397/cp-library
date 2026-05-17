9b9f0de4d496c8a3299a4c053fee01c85ac6eb57
sourav739397 <sourav739397@gmail.com>
2026-05-17 11:41:34 +0600

    update

--- content/number-theory/Primality/Sieve.h
+++ content/number-theory/Primality/Sieve.h
@@ -1,27 +1,27 @@
 /**
- * Description: Tests primality up to $SZ$. Runs faster if only
+ * Description: Tests primality up to $N$. Runs faster if only
  	* odd indices are stored.
- * Time: O(SZ\log\log SZ) or O(SZ)
+ * Time: O(N\log\log N) or O(N)
  * Source: KACTL 
  * Verification: https://open.kattis.com/problems/primesieve
  */
 
-template<int SZ> struct Sieve { 
-	bitset<SZ> is_prime; vi primes;
+template<int N> struct Sieve { 
+	bitset<N> is_prime; vector<int> primes;
 	Sieve() {
 		is_prime.set(); is_prime[0] = is_prime[1] = 0;
-		for (int i = 4; i < SZ; i += 2) is_prime[i] = 0;
-		for (int i = 3; i*i < SZ; i += 2) if (is_prime[i])
-			for (int j = i*i; j < SZ; j += i*2) is_prime[j] = 0;
-		F0R(i,SZ) if (is_prime[i]) primes.pb(i);
+		for (int i = 4; i < N; i += 2) is_prime[i] = 0;
+		for (int i = 3; i*i < N; i += 2) if (is_prime[i])
+			for (int j = i*i; j < N; j += i*2) is_prime[j] = 0;
+		for (int i = 0; i < N; i++) if (is_prime[i]) primes.push_back(i);
 	}
-	// int sp[SZ]{}; // smallest prime that divides
+	// array<int, N> spf{} // smallest prime that divides
 	// Sieve() { // above is faster
-	// 	FOR(i,2,SZ) { 
-	// 		if (sp[i] == 0) sp[i] = i, primes.pb(i); 
+	// 	for (int i = 2; i < N; i++) { 
+	// 		if (spf[i] == 0) spf[i] = i, primes.push_back(i); 
 	// 		for (int p: primes) {
-	// 			if (p > sp[i] || i*p >= SZ) break;
-	// 			sp[i*p] = p;
+	// 			if (p > spf[i] || i*p >= N) break;
+	// 			spf[i*p] = p;
 	// 		}
 	// 	}
 	// }
