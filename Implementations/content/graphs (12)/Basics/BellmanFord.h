dd26e4e0937d673905af9c059315e5dcfa0b9f40
sourav739397 <sourav739397@gmail.com>
2026-05-17 19:03:58 +0600

    .

--- Implementations/content/graphs (12)/Basics/BellmanFord.h
+++ Implementations/content/graphs (12)/Basics/BellmanFord.h
@@ -12,37 +12,28 @@ struct BellmanFord {
   vector<array<int, 3>> ed;
   BellmanFord(int _N) {
     N = _N;
-		adj.resize(N);
-		dist.resize(N, INT64_MAX);
-	}
-	void ae(int u, int v, int w)
-	{
-		adj[u].push_back(v);
-		ed.push_back({u, v, w});
-	}
-	void genBad(int u)
-	{
-		if (dist[u] == INT64_MIN)
-			return;
-		dist[u] = INT64_MIN;
-		for (auto &v : adj[u])
-			genBad(v);
-	}
-	void genDist(int src)
-	{
-		dist[src] = 0;
-		for (int i = 0; i < N; i++)
-		{
-			for (auto &[u, v, w] : ed)
-			{
-				if (dist[u] < INT64_MAX)
-				{
-					dist[v] = min(dist[v], dist[u] + w);
+    adj.resize(N);
+    dist.resize(N, INT64_MAX);
+  }
+  void ae(int u, int v, int w) {
+    adj[u].push_back(v);
+    ed.push_back({u, v, w});
+  }
+  void genBad(int u) {
+    if (dist[u] == INT64_MIN) return;
+    dist[u] = INT64_MIN;
+    for (auto &v : adj[u]) genBad(v);
+  }
+  void genDist(int src) {
+    dist[src] = 0;
+    for (int i = 0; i < N; i++) {
+      for (auto &[u, v, w] : ed) {
+        if (dist[u] < INT64_MAX) {
+          dist[v] = min(dist[v], dist[u] + w);
 				}
 			}
 		}
-		for (auto &[u, v, w] : ed)
-		{
+    for (auto &[u, v, w] : ed) {
 			if (dist[u] < INT64_MAX && dist[v] > dist[u] + w)
 				genBad(v);
 		}
