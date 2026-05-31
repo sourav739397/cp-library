4784fad25a17515804fe69f6eeb263738d4ef29a
sourav739397 <sourav739397@gmail.com>
2026-05-31 22:47:30 +0600

    .

--- Implementations/content/data-structures/Static Range Queries/SparseTable.h
+++ Implementations/content/data-structures/Static Range Queries/SparseTable.h
@@ -11,21 +11,21 @@
 **/
 
 template<class T> struct SparseTable {
-  int N, LOG;
-  vector<vector<T>> jmp;
-  T cmb(T a, T b) { return min(a, b); }
-  SparseTable(const vector<T>& v) {
-    N = v.size(); LOG = __lg(N);
-    jmp.resize(N, vector<T>(LOG + 1));
-    for (int i = 0; i < N; ++i) jmp[i][0] = v[i];
-    for (int j = 1; j <= LOG; ++j) {
-      for (int i = 0; i + (1<<j) <= N; ++i) {
-        jmp[i][j] = cmb(jmp[i][j-1], jmp[i+(1<<(j-1))][j-1]);
-      }
-    }
-  }
-  T query(int l, int r) {
-    int d = __lg(r-l+1);
-    return cmb(jmp[l][d], jmp[r-(1<<d)+1][d]);
-  }
+	int N, LOG;
+	vector<vector<T>> jmp;
+	T cmb(T a, T b) { return min(a, b); }
+	SparseTable(const vector<T>& v) {
+		N = v.size(); LOG = __lg(N);
+		jmp.resize(N, vector<T>(LOG + 1));
+		for (int i = 0; i < N; ++i) jmp[i][0] = v[i];
+		for (int j = 1; j <= LOG; ++j) {
+			for (int i = 0; i + (1<<j) <= N; ++i) {
+				jmp[i][j] = cmb(jmp[i][j-1], jmp[i+(1<<(j-1))][j-1]);
+			}
+		}
+	}
+	T query(int l, int r) {
+		int d = __lg(r-l+1);
+		return cmb(jmp[l][d], jmp[r-(1<<d)+1][d]);
+	}
 };
